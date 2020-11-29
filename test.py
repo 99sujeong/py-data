29a66d0e66ae5a8206d49ea639e88209b525fa23
99sujeong <66475575+99sujeong@users.noreply.github.com>
2020-11-29 23:07:44 +0900

    anaconda test

---
+++ test.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import matplotlib.pyplot as plt
+'''
+plt.title('color')
+plt.plot([10, 20, 30, 40], color='skyblue', label='skyblue')
+plt.plot([40, 30, 20, 10], 'pink', label='pink')
+plt.legend()
+plt.show()
+'''
+plt.hist([1, 1, 2, 3, 4, 5, 6, 6, 7, 8, 10])
+plt.show()
