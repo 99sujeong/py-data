b3a322f0c664b43556897315a832b387441583b0
99sujeong <66475575+99sujeong@users.noreply.github.com>
2020-12-02 22:39:24 +0900

    demo-file open

---
+++ final-demo.py
@@ -0,0 +1,22 @@
+import csv
+f = open('populationbyage.csv')
+data = csv.reader(f)
+next(data); next(data)
+
+result = []
+total10 = [];total20 = [];total30 = [];total40 = [];total50 = [];total60 = []
+
+'''
+for row in data:
+    row[4:5] = map(int, row[4:5])
+    result.append(row[3])
+ 
+
+print(result)
+''' 
+
+for row in data:
+    row[1:] = map(int, row[1:])
+    for i in range(6):
+        a = row[i * 5 + 3]
+        print(a)
